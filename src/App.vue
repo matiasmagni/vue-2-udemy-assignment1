883c35cb5850246ae66d8f6121a3e01956552f04
Matías J. Magni <matias.magni@gmail.com>
2018-02-19 12:18:23 -0300

    APVL-2: First commit. Assignment 1 completed.

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <Excercise1 />
+  </div>
+</template>
+
+<script>
+import Excercise1 from './components/Excercise1'
+
+export default {
+  name: 'App',
+  components: {
+    Excercise1
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Excercise1.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <!-- 1) Fill the <p> below with your Name and Age - using Interpolation -->
+    <p>VueJS is pretty cool - {{name}}</p>
+    <!-- 2) Output your age, multiplied by 3 -->
+    <p>Age (x3): {{age}}</p>
+    <!-- 3) Call a function to output a random float between 0 and 1 (Math.random()) -->
+    <p>Random Number between 0 and 1: {{ getRandomNum() }}</p>
+    <!-- 4) Search any image on Google and output it here by binding the "src" attribute -->
+    <div>
+        <img v-bind:src="image" alt="Google's image" />
+    </div>
+    <!-- 5) Pre-Populate this input with your name (set the "value" attribute) -->
+    <div>
+        <input type="text" v-bind:value="name" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Excercise1',
+  data () {
+    return {
+      name: 'Matías J. Magni',
+      age: 31 * 3,
+      image: 'https://ciesbizblog.files.wordpress.com/2013/12/internetexplorersucks_88340a_3358046.jpg'
+    }
+  },
+  methods: {
+    getRandomNum () {
+      return Math.random()
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
